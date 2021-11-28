1874fbd37131e21d6fb62065f6d2031ba823af5a
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-11-29 01:37:35 +0900

    fix: gradleのコンフリクトエラーが残っていたため解消＆エミュレーターで動作確認

--- lib/matching.dart
+++ lib/matching.dart
@@ -1,48 +1,50 @@
 import 'package:flutter/material.dart';
 
 class MatchingPage extends StatelessWidget {
-  MatchingPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        // padding: const EdgeInsets.all(0.5),
-        height: 580,
-        width: 320,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-            image: AssetImage('images/obachan.jpg'),
-            fit: BoxFit.cover,
+    return Scaffold(
+      body: Center(
+        child: Container(
+          // padding: const EdgeInsets.all(0.5),
+          height: 580,
+          width: 320,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: AssetImage('assets/sample.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('title',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 100.0,
+                  )),
+              //child: Column(Text()),
+              Text('post',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 80.0,
+                  )),
+            ],
           ),
         ),
-
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'title',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 100.0,
-              )
-            ),
-            //child: Column(Text()),
-            Text(
-                'post',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 80.0,
-                )
-            ),
-          ],
-        ),
+      ),
+      floatingActionButton: GestureDetector(
+        onTap: () {
+          {
+            Navigator.pop(context);
+          }
+        },
       ),
     );
   }
 }
-
